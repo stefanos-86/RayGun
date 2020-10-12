15288932f211676887a0706a7c035f7d60f7ee30
Stefano <stefanos_86@libero.it>
2020-10-12 17:58:30 +0200

    Rename the wall hit to make it clear that it is the wall hit.

--- RayCast/ProjectionPlane.cpp
+++ RayCast/ProjectionPlane.cpp
@@ -38,26 +38,25 @@ namespace rc {
 		
 		for (uint16_t scan_column = 0; scan_column < columns; ++scan_column) {
 			r.alpha_rad = normalize_0_2pi(r.alpha_rad);
-			RayHit hit = grid.cast_ray(r);
+			RayHit wall_hit = grid.cast_ray(r);
 
 			const float fishbowl = std::cos(original_ray_orientation - r.alpha_rad);
 
-			if (hit.really_hit()) {
-				hit.distance *= fishbowl;
+			if (wall_hit.really_hit()) {
+				wall_hit.distance *= fishbowl;
 				
-				const SliceProjection wall_projection = project_slice(hit.distance, grid.cell_size);
-				c.draw_slice(scan_column, wall_projection.top_row, wall_projection.height, hit.offset, TextureIndex::WALL);
+				const SliceProjection wall_projection = project_slice(wall_hit.distance, grid.cell_size);
+				c.draw_slice(scan_column, wall_projection.top_row, wall_projection.height, wall_hit.offset, TextureIndex::WALL);
 			}
 
 
 			for (const Sprite& testSprite : world.enemies.sprites) {  //TODO: use an intersection method at collection level to account for z-order.
 				RayHit enemy_hit = testSprite.intersection(r);
 				if (enemy_hit.really_hit() &&  // Don't bother if there's not it.
-   				   (hit.no_hit() || enemy_hit.distance < hit.distance))  // Depth test: either no wall, either in front of it.
+   				   (wall_hit.no_hit() || enemy_hit.distance < wall_hit.distance))  // Depth test: either no wall, either in front of it.
 				{
 					enemy_hit.distance *= fishbowl;
 
-					// TODO Try to see if the same projection works... at least until sprite and cell are the same size.
 					const SliceProjection enemy_projection = project_slice(enemy_hit.distance, testSprite.size);
 					c.draw_slice(scan_column, enemy_projection.top_row, enemy_projection.height, enemy_hit.offset, TextureIndex::ENEMY);
 
