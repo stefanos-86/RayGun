af1ebd17c313a7af0593f814cbba0b9af924629a
Stefano <stefanos_86@libero.it>
2020-11-08 10:33:35 +0100

    Extract the mock canvas in its own header, in preparation for reuse in other tests.

---
+++ RayCastTest/MockCanvas.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Canvas.h"
+
+namespace rc {
+
+    class MockCanvas : public Canvas {
+    public:
+        void draw_slice(const uint16_t column, const int16_t top_row, const uint16_t height, const uint16_t texture_offset, const TextureIndex what_to_draw) final {
+            column_calls.push_back(column);
+            top_row_calls.push_back(top_row);
+            height_calls.push_back(height);
+        }
+
+        bool transparent_pixel(const uint8_t x, const uint8_t y, const TextureIndex image) const final {
+            return false;
+        }
+
+        void draw_text(const std::string& text, uint16_t row, const int16_t column, const uint8_t font_size) const final {
+            // Never used.
+        }
+
+        std::vector<uint16_t> column_calls;
+        std::vector<uint16_t> top_row_calls;
+        std::vector<uint16_t> height_calls;
+    };
+}
