b5ae5f0c729def4628dc29539a84d7c891949ef7
Stefano <stefanos_86@libero.it>
2020-11-17 19:25:21 +0100

    Ruin the gun image enough so that it does not look too much out of place.

--- RayCast/Hud.cpp
+++ RayCast/Hud.cpp
@@ -14,7 +14,7 @@ namespace rc {
 		onscreen_message << "AMMO:" << std::setfill('0') << std::setw(2) << (int) player.bullets_left;
 		onscreen_message << "  KILLS:" << std::setw(3) << (int)player.kills;
 
-		c.draw_image((640-82)/2, 480 - 160, TextureIndex::HUD);  // TODO Some kind of "automatically to bottom/center" functionality needed.
+		c.draw_image((640-64)/2, 480 - 64, TextureIndex::HUD);  // TODO Some kind of "automatically to bottom/center" functionality needed.
 		c.draw_text(onscreen_message.str(), 10, 10, 16); // TODO: position/size should depend on screen size, not be hardcoded.
 	}
 	void Hud::alert_pause(Canvas& c) const
