8254ab7de90d852fa11cd838e15cde95c549bda3
Stefano <stefanos_86@libero.it>
2020-12-12 13:03:09 +0100

    Add the file with the index of sounds.

---
+++ RayCast/Loudspeaker.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+
+namespace rc {
+
+	enum class SoundIndex : uint8_t {
+		GUN_SHOT,
+		MUSIC_CALM,
+		MUSIC_SLOW,
+		MUSIC_MID,
+		MUSIC_FAST
+	};
+
+	class Loudspeaker {
+
+	};
+
+}
